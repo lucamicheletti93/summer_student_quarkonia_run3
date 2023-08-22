0ed63e05fdf4c37c31d61af05d036c38ef191f14
LukVic <109143242+LukVic@users.noreply.github.com>
2023-08-22 16:56:13 +0200

    tauz promp fit update
    tauz promp fit update

--- macro/tauz_prompt_fit.py
+++ macro/tauz_prompt_fit.py
@@ -1,39 +1,38 @@
 import ROOT
 
-PATH_DATA = "root_files/"
-PATH_IMGS = "imgs/"
+PATH_DATA = "/afs/cern.ch/user/l/lvicenik/private/summer_student_quarkonia_run3/root_files/"
+PATH_IMGS = "/afs/cern.ch/user/l/lvicenik/private/summer_student_quarkonia_run3/imgs/"
 #PATH_WORKSPACE = "/afs/cern.ch/user/l/lvicenik/private/summer_student_quarkonia_run3/workspaces/"
 
-ht_data_cut_file = ROOT.TFile(PATH_DATA + "template_tune_45_6-10.root")
+ht_data_cut_file = ROOT.TFile(PATH_DATA + "template_tune_45_4-6.root")
 ht_data = ht_data_cut_file.Get("ht")
-ht_data.Scale(1. / ht_data.Integral())
+#ht_data.Scale(1. / ht_data.Integral())
 
 tauz = ROOT.RooRealVar("Dimuon tauz", "Dimuon pseudoproper decay length", -0.007, 0.007)
 
 datahist_data_t = ROOT.RooDataHist("datahisttdatabg", "DataHist t data bg", ROOT.RooArgList(tauz), ht_data)
 
 mean = ROOT.RooRealVar("mean", "Mean", 0.0, -1, 1)
-width = ROOT.RooRealVar("width", "Width", 0.001, -0.01, 10)
+width = ROOT.RooRealVar("width", "Width", 0.001, 0.0, 10)
 bw_pdf = ROOT.RooBreitWigner("bw", "Breit-Wigner Distribution", tauz, mean, width)
 
-# mean = ROOT.RooRealVar("mean", "Mean", 0.0, -0.1, 0.1)
-# sigma = ROOT.RooRealVar("sigma", "Sigma", 0.01, 0.0, 2)
-# gauss_pdf = ROOT.RooGaussian("gaussian", "Gaussian Distribution", tauz, mean, sigma)
+mean_gauss = ROOT.RooRealVar("mean_gauss", "Mean", 0.0, -0.1, 0.1)
+sigma_gauss = ROOT.RooRealVar("sigma_gauss", "Sigma", 0.001, 0.0, 10)
+gauss_pdf = ROOT.RooGaussian("gaussian", "Gaussian Distribution", tauz, mean_gauss, sigma_gauss)
 
-# Create variables for the mass and parameters of the Crystal Ball function
+#Create variables for the mass and parameters of the Crystal Ball function
 # mean = ROOT.RooRealVar("mean", "Mean", 0, 0, 1)
 # sigma = ROOT.RooRealVar("sigma", "Sigma", 0.04, 0.0001, 1.0)
-# alpha = ROOT.RooRealVar("alpha", "Alpha", 1.0, 0.5, 10.0)
+# alpha = ROOT.RooRealVar("alpha", "Alpha", 1.0, -10, 10.0)
 # n = ROOT.RooRealVar("n", "n", 1.0, 0.1, 10.0)
-
-# # Create the Crystal Ball PDF
+# # # Create the Crystal Ball PDF
 # cb_pdf = ROOT.RooCBShape("crystal_ball", "Crystal Ball PDF", tauz, mean, sigma, alpha, n)
 
-cheb_coeffs = [ROOT.RooRealVar(f"cheb_coeff_{i}", f"Coeff_{i}", 0.01, -0.2, 0.2) for i in range(3)]
-cheb_poly = ROOT.RooChebychev("cheb_poly", "Chebyshev Polynomial", tauz, ROOT.RooArgList(*cheb_coeffs))
+# cheb_coeffs = [ROOT.RooRealVar(f"cheb_coeff_{i}", f"Coeff_{i}", 0.01, -0.31, 0.31) for i in range(4)]
+# cheb_poly = ROOT.RooChebychev("cheb_poly", "Chebyshev Polynomial", tauz, ROOT.RooArgList(*cheb_coeffs))
 
 model_frac = ROOT.RooRealVar("cb_frac", "CB Fraction", 0.5, 0, 1)
-model_t = ROOT.RooAddPdf("model_prompt_pdf", "Breit Wiegner + Chebyshev", ROOT.RooArgList(bw_pdf, cheb_poly), ROOT.RooArgList(model_frac))
+model_t = ROOT.RooAddPdf("model_prompt_pdf", "Breit Wiegner + Chebyshev", ROOT.RooArgList(bw_pdf, gauss_pdf), ROOT.RooArgList(model_frac))
 
 model_t.fitTo(datahist_data_t)
 
@@ -49,8 +48,8 @@ canvas.Draw()
 
 canvas.SaveAs(PATH_IMGS + "tauz_template_prompt_fit.png")
 
-tauz_prompt_fit_workspace = ROOT.RooWorkspace("tauz_prompt_fit_workspace")
-tauz_prompt_fit_workspace.Import(model_t)
+# tauz_prompt_fit_workspace = ROOT.RooWorkspace("tauz_prompt_fit_workspace")
+# tauz_prompt_fit_workspace.Import(model_t)
 
 #workspace_file = ROOT.TFile(PATH_WORKSPACE + "tauz_prompt_bw_cheb_fit_workspace.root", "RECREATE")
 #tauz_prompt_fit_workspace.Write()
